268a987a137d4b8c6ad3cb42b12a0ca90e7f4f4e
ishland <ishlandmc@yeah.net>
2024-07-30 18:29:28 +0800

    change: make `DynamicPriorityQueue` mostly parallel capable

--- src/main/java/com/ishland/flowsched/structs/DynamicPriorityQueue.java
+++ src/main/java/com/ishland/flowsched/structs/DynamicPriorityQueue.java
@@ -1,81 +1,85 @@
 package com.ishland.flowsched.structs;
 
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-import it.unimi.dsi.fastutil.objects.ObjectLinkedOpenHashSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A priority queue with fixed number of priorities and allows changing priorities of elements.
- * Not thread-safe.
  *
  * @param <E> the type of elements held in this collection
  */
 public class DynamicPriorityQueue<E> {
 
-    private final ObjectLinkedOpenHashSet<E>[] priorities;
-    private final Object2IntMap<E> priorityMap = new Object2IntOpenHashMap<>();
+    private final ConcurrentLinkedQueue<E>[] priorities;
+    private final ConcurrentHashMap<E, Integer> priorityMap = new ConcurrentHashMap<>();
 
-    private int currentMinPriority = 0;
+    private AtomicInteger currentMinPriority = new AtomicInteger(0);
 
     public DynamicPriorityQueue(int priorityCount) {
         //noinspection unchecked
-        this.priorities = new ObjectLinkedOpenHashSet[priorityCount];
+        this.priorities = new ConcurrentLinkedQueue[priorityCount];
         for (int i = 0; i < priorityCount; i++) {
-            this.priorities[i] = new ObjectLinkedOpenHashSet<>();
+            this.priorities[i] = new ConcurrentLinkedQueue<>();
         }
     }
 
-    public synchronized void enqueue(E element, int priority) {
+    public void enqueue(E element, int priority) {
         if (priority < 0 || priority >= priorities.length)
             throw new IllegalArgumentException("Priority out of range");
-        if (priorityMap.containsKey(element))
+        if (this.priorityMap.putIfAbsent(element, priority) != null)
             throw new IllegalArgumentException("Element already in queue");
 
-        if (!priorities[priority].add(element)) throw new AssertionError("Element already in priority " + priority);
-        priorityMap.put(element, priority);
-        if (priority < currentMinPriority)
-            currentMinPriority = priority;
+        this.priorities[priority].add(element);
+        this.currentMinPriority.getAndUpdate(operand -> Math.min(operand, priority));
     }
 
-    public synchronized boolean changePriority(E element, int priority) {
+    // behavior is undefined when changing priority for one item concurrently
+    public boolean changePriority(E element, int priority) {
         if (priority < 0 || priority >= priorities.length)
             throw new IllegalArgumentException("Priority out of range");
-        if (!priorityMap.containsKey(element)) return false; // ignored
 
-        int oldPriority = priorityMap.getInt(element);
-        if (oldPriority == priority) return false; // nothing to do
-
-        if (!priorities[oldPriority].remove(element)) throw new AssertionError("Element not found in priority " + oldPriority);
-        if (!priorities[priority].addAndMoveToLast(element)) throw new AssertionError("Element already in priority " + priority);
-        priorityMap.put(element, priority);
-
-        if (priority < currentMinPriority) currentMinPriority = priority;
+        int currentPriority = this.priorityMap.getOrDefault(element, -1);
+        if (currentPriority == -1 || currentPriority == priority) {
+            return false; // a clear failure
+        }
+        final boolean removedFromQueue = this.priorities[currentPriority].remove(element);
+        if (!removedFromQueue) {
+            return false; // the element is dequeued while we are changing priority
+        }
+        final Integer put = this.priorityMap.put(element, priority);
+        final boolean changeSuccess = put != null && put == currentPriority;
+        if (!changeSuccess) {
+            return false; // something else may have called remove()
+        }
+        this.priorities[priority].add(element);
+        this.currentMinPriority.getAndUpdate(operand -> Math.min(operand, priority));
         return true;
     }
 
-    public synchronized E dequeue() {
-        while (currentMinPriority < priorities.length) {
-            ObjectLinkedOpenHashSet<E> priority = this.priorities[currentMinPriority];
-            if (priority.isEmpty()) {
-                currentMinPriority++;
-                continue;
+    public E dequeue() {
+        int current = this.currentMinPriority.get();
+        while (current < this.priorities.length) {
+            E element = priorities[current].poll();
+            if (element != null) {
+                this.priorityMap.remove(element);
+                return element;
+            } else {
+                int finalCurrent = current;
+                current = this.currentMinPriority.updateAndGet(operand -> operand < this.priorities.length ? Math.min(operand + (priorities[operand].isEmpty() ? 1 : 0), finalCurrent + 1) : operand);
             }
-            E element = priority.removeFirst();
-            priorityMap.removeInt(element);
-            return element;
         }
         return null;
     }
 
-    public synchronized boolean contains(E element) {
+    public boolean contains(E element) {
         return priorityMap.containsKey(element);
     }
 
-    public synchronized void remove(E element) {
-        if (!priorityMap.containsKey(element))
-            return; // ignore
-        int priority = priorityMap.removeInt(element);
-        if (!priorities[priority].remove(element)) throw new AssertionError("Element not found in priority " + priority);
+    public void remove(E element) {
+        final Integer remove = this.priorityMap.remove(element);
+        if (remove == null) return;
+        this.priorities[remove].remove(element); // best-effort
     }
 
     public int size() {
